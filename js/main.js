6ba4feffdb9c0916973c51233278ba8dee2557b8
Jorge Rojas <jorgearojas25@gmail.com>
2020-03-23 01:59:08 -0500

    3/23/2020 index V1

--- js/main.js
+++ js/main.js
@@ -1,26 +1,52 @@
 const options = document.getElementsByClassName("searchOptions")[0];
+const searchButton = document.getElementById('searchButton');
+const ListaTemas = document.getElementById('themesList');
+const TagsSearch = document.getElementById('tagButtons');
+const InputSearch = document.getElementById('userSearch');
+let listaTemasAbierta = false;
+
+const OpenThemeList = () => {
+  listaTemasAbierta = !listaTemasAbierta;
+  if(listaTemasAbierta){
+    console.log('hola')
+    
+    ListaTemas.setAttribute('style', 'display: flex')
+  
+  }else{
+    ListaTemas.setAttribute('style', 'display: none')
+  }
+}
 
 const SearchGifs = search => {
-  return fetch(
-    gyphyObject.createUrl(new Consulta(UrlTypes.search, 12, search))
-  )
-    .then(data => {
-      return data.json();
-    })
-    .then(data => {
-      console.log(data);
-      OcultarArea('tendencesArea');
-      CreateImageContainerInZone("resultsGifs", data.data);
-      options.setAttribute('style', 'display: none;')
-    })
-    .catch(e => {
-      console.error(`[Search bar] ${e}`);
-    });
+  if(search.length){
+    return fetch(
+      gyphyObject.createUrl(new Consulta(UrlTypes.search, 12, search))
+    )
+      .then(data => {
+        return data.json();
+      })
+      .then(data => {
+        console.log(data);
+        OcultarMostrarArea('tendencesArea',true);
+        OcultarMostrarArea('suggestArea',true);
+        OcultarMostrarArea('searchArea',false);
+        CrearBotonesTag(data.data[0]);
+        let text = document.getElementById('searchedText');
+        text.innerHTML = search;
+        InputSearch.value = search;
+        CreateImageContainerInZone("resultsGifs", data.data, true);
+        options.setAttribute('style', 'display: none;')
+      })
+      .catch(e => {
+        console.error(`[Search bar] ${e}`);
+      });
+  }
+  return null
 };
 
-const OcultarArea = (area) => {
+const OcultarMostrarArea = (area, ocultar) => {
   let areaGifs = document.getElementById(area);
-  areaGifs.setAttribute('style', 'display: none;')
+  areaGifs.setAttribute('style', `display: ${ocultar? 'none': 'block'};`)
 }
 
 const DisplaySearchOtions = async text => {
@@ -34,18 +60,25 @@ const DisplaySearchOtions = async text => {
         return data.json();
       })
       .catch(e => console.error(`[Display Search Options] ${e}`));
-    let Titulo = ObtenerDatosTitulo(data.data[0]);
-    Titulo.forEach(element => {
-      let newButton = document.createElement("button");
-      newButton.setAttribute("class", "optionSearch");
-      newButton.innerHTML += element;
-      options.appendChild(newButton);
-    });
+      CrearOptionsButtons(data);
+    searchButton.setAttribute('class', 'searchButton outline')
     options.setAttribute("style", "display: flex");
   } else {
     options.setAttribute("style", "display: none");
+    searchButton.setAttribute('class', 'searchButton')
   }
 };
+
+const CrearOptionsButtons = (data) => {
+  let Titulo = ObtenerDatosTitulo(data.data[0]);
+    Titulo.forEach(element => {
+      let newButton = document.createElement("button");
+      newButton.setAttribute("class", "optionSearch");
+      newButton.setAttribute("onClick", `SearchGifs('${element}')`);
+      newButton.innerHTML += element;
+      options.appendChild(newButton);
+    });
+}
 
 fetch(
   gyphyObject.createUrl(new Consulta(UrlTypes.trending, 16, null))
@@ -54,26 +87,23 @@ fetch(
     return data.json();
   })
   .then(data => {
-    let dataCompleta = data.data;
-    console.log(dataCompleta);
-    let x = dataCompleta.splice(-4);
-    CreateImageContainerInZone('suggestGifs',x)
-    console.log(x)
-    console.log(dataCompleta);
-    CreateImageContainerInZone("tendencesGifs", dataCompleta);
+    let dataTendencias = data.data;
+    let dataSuggest = dataTendencias.splice(-4);
+    CreateImageContainerInZone('suggestGifs',dataSuggest, false);
+    CreateImageContainerInZone("tendencesGifs", dataTendencias, true);
   })
   .catch(e => {
     console.error(`[Search bar] ${e}`);
   });
 
-const CreateImageContainerInZone = async (imageZone, dataArray) => {
+const CreateImageContainerInZone = async (imageZone, dataArray, gridDinamico) => {
   let zone = document.getElementById(imageZone);
   zone.innerHTML = "";
   console.log(zone);
   for (const image of dataArray) {
     let imgTitleSrc = ObtenerDatosTitulo(image);
     //container
-    let imageContainer = CrearContainerImage(image);
+    let imageContainer = CrearContainerImage(image, gridDinamico);
     //Titulo
     let title = CreatTituloImagen(imgTitleSrc);
     //imagen
@@ -91,10 +121,6 @@ const CreateImageContainerInZone = async (imageZone, dataArray) => {
     zone.append(imageContainer);
   }
 };
-
-
-
-
 const ObtenerDatosTitulo = gifImage => {
   if (gifImage.title) {
     let imgTitleSrc = gifImage.title.split(" ");
@@ -106,14 +132,22 @@ const ObtenerDatosTitulo = gifImage => {
   return [];
 };
 
-const CrearContainerImage = image => {
+const CrearContainerImage = (image, esDinamico) => {
   let sizeSrcImg = image.images.downsized_large.width;
   let imageContainer = document.createElement("div");
   imageContainer.setAttribute("class", "imageContainer");
-  imageContainer.setAttribute(
-    "style",
-    `grid-column: ${sizeSrcImg >= 500 ? "span 2" : "span 1"};`
-  );
+  if(esDinamico){
+    imageContainer.setAttribute(
+      "style",
+      `grid-column: ${sizeSrcImg >= 500 ? "span 2" : "span 1"};`
+    );
+  }else{
+    imageContainer.setAttribute(
+      "style",
+      `grid-column: span 1;`
+    );
+  }
+  
   return imageContainer;
 };
 
@@ -135,13 +169,14 @@ const CreatTituloImagen = imgTitleSrc => {
 const CrearImagenGif = image => {
   let imageContent = document.createElement("img");
   imageContent.setAttribute("src", `${image.images.downsized_large.url}`);
+  imageContent.setAttribute('class', 'imageGif');
   return imageContent;
 };
 
 const CrearButtonMore = imgTitleSrc => {
   let buttonMore = document.createElement("button");
     buttonMore.setAttribute("class", "buttonMore");
-    buttonMore.setAttribute('value', `${imgTitleSrc[0]}`);
+    buttonMore.setAttribute("onClick", `SearchGifs('${imgTitleSrc[0]}')`);
     buttonMore.innerHTML = "Ver más…";
     return buttonMore;
 }
@@ -154,5 +189,17 @@ const CrearTagVar = imgTitleSrc => {
     tagContent.innerHTML = `#${imgTitleSrc.join(" #")}`;
     tagVar.appendChild(tagContent);
     return tagVar;
+}
+
+const CrearBotonesTag = (data) => {
+  TagsSearch.innerHTML = ""; 
+  let Tags = ObtenerDatosTitulo(data);
+  Tags.forEach(element => {
+    let newButton = document.createElement("button");
+    newButton.setAttribute("class", "tagButton");
+    newButton.setAttribute("onClick", `SearchGifs('${element}')`);
+    newButton.innerHTML += `#${element}`; 
+    TagsSearch.appendChild(newButton);
+  });
 }
 
